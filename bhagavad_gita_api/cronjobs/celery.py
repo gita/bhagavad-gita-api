35011dfe930e0f94abcd2a1abb2d6e127b2d7264
sreevardhanreddi <sreevardhanreddi@gmail.com>
2021-09-22 12:49:58 +0530

    udpated docker-compose files, added celery beat and updated deploy workflows

--- bhagavad_gita_api/cronjobs/celery.py
+++ bhagavad_gita_api/cronjobs/celery.py
@@ -1,3 +1,6 @@
+import datetime
+
+import pytz
 import requests
 from celery import Celery
 from celery.schedules import crontab
@@ -6,32 +9,29 @@ from bhagavad_gita_api.config import settings
 
 app = Celery(
     "cronjobs",
-    broker=settings.REDIS_BROKER,
-    backend=settings.REDIS_BACKEND,
+    broker=settings.CELERY_BROKER,
+    backend=settings.CELERY_BACKEND,
 )
-
-
-app.conf.timezone = "UTC"
-
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-
-    sender.add_periodic_task(
-        crontab(minute=0, hour=0),
-        set_verse.s(),
-    )
+app.conf.timezone = "Asia/Calcutta"
 
 
 @app.task
 def set_verse():
-    url = "{}/v2/set-daily-verse".format(settings.CRONJOB_BASE_URL)
+    url = "{}/v2/set-daily-verse/".format(settings.CRONJOB_BASE_URL)
     data = {
         "accept": "application/json",
-        "X-API-KEY": settings.X_API_KEY,
+        "X-API-KEY": settings.TESTER_API_KEY,
     }
     r = requests.post(url=url, data=data, headers=data)
     print(r)
+
+
+app.conf.beat_schedule = {
+    "setup-verse-everyday": {
+        "task": "bhagavad_gita_api.cronjobs.celery.set_verse",
+        "schedule": crontab(hour=0, minute=0),
+    },
+}
 
 
 if __name__ == "__main__":
