56d8314bf2fa3b211b10893d0cc9b5d86405d125
Srishtikumari2002 <srishti060btcsai20@igdtuw.ac.in>
2021-12-22 13:36:19 +0530

    feat--Sanskrit  audio recitation

--- bhagavad_gita_api/config.py
+++ bhagavad_gita_api/config.py
@@ -69,7 +69,7 @@ settings = Settings()
 if not settings.SQLALCHEMY_DATABASE_URI:
     print(
         "No SQLALCHEMY_DATABASE_URI found. \
-        \nUsing default set Sqlite database gita.db. This is not good for running in production!"
+        \nUsing in-file Sqlite database. This is not good for running in production!"
     )
     settings.SQLALCHEMY_DATABASE_URI = "sqlite:///{}?{}".format(
         os.path.join(os.path.dirname(os.path.realpath(__file__)), "gita.db"),
