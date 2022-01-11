c2e465bd76a06fd4582868028bafbae9a641b9df
Gupta-Anubhav12 <anubhavgupta2260@gmail.com>
2022-01-11 11:55:05 +0530

    social bot support

--- bhagavad_gita_api/api/api_v2/endpoints/social.py
+++ bhagavad_gita_api/api/api_v2/endpoints/social.py
@@ -2,10 +2,11 @@ import logging
 from datetime import date
 
 from fastapi import APIRouter, Depends, HTTPException, Response
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 
 from bhagavad_gita_api.api import deps
 from bhagavad_gita_api.models import gita as models
+from bhagavad_gita_api.utils import post_on_instagram
 
 logger = logging.getLogger("api")
 logger.setLevel(logging.DEBUG)
@@ -13,7 +14,7 @@ logger.setLevel(logging.DEBUG)
 router = APIRouter()
 
 
-@router.post("/post-on-instagram/", tags=["social"])
+@router.post("/post_verse_of_the_day/", tags=["social"])
 async def post_instagram(db: Session = Depends(deps.get_db)):
     verse_of_day = (
         db.query(models.VerseOfDay)
@@ -26,18 +27,24 @@ async def post_instagram(db: Session = Depends(deps.get_db)):
     if verse_of_day:
         verse = (
             db.query(models.GitaVerse)
-            .options(
-                joinedload(models.GitaVerse.translations),
-            )
+            .join(models.GitaTranslation)
             .filter(models.GitaVerse.id == verse_of_day.verse_order)
             .first()
         )
 
         if verse:
+
             # CALL INSTAGRAM POSTING FUNCTION HERE
-            return Response(
-                status_code=201,
-                content="Verse of the day has been posted on Instagram.",
+            translations = db.query(models.GitaTranslation).filter(
+                models.GitaTranslation.verse_id == verse.id
             )
+            response = post_on_instagram(verse, translations)
+            if response == 200:
+                return Response(
+                    status_code=201,
+                    content="Verse of the day has been posted on Instagram., response from bot : {}".format(
+                        response
+                    ),
+                )
 
     raise HTTPException(status_code=404, detail="Verse of the day not found.")
--- bhagavad_gita_api/utils.py
+++ bhagavad_gita_api/utils.py
@@ -1,8 +1,13 @@
 import datetime
 
 import pytz
+from myigbot import MyIGBot
+from PIL import Image, ImageDraw, ImageFont
 from sqlalchemy import DateTime
 from sqlalchemy.types import TypeDecorator
+from textwrap3 import wrap
+
+from bhagavad_gita_api.models import gita as models
 
 
 def tzware_datetime():
@@ -32,3 +37,84 @@ class AwareDateTime(TypeDecorator):
 
     def __repr__(self):
         return "AwareDateTime()"
+
+
+def post_on_instagram(verse, translations):
+
+    # print(verse)
+    text = (
+        translations.filter(models.GitaTranslation.author_name == "Swami Sivananda")
+        .first()
+        .description
+    )
+
+    hindi_text = (
+        translations.filter(models.GitaTranslation.author_name == "Swami Ramsukhdas")
+        .first()
+        .description
+    )
+    sanskrit_text = verse.text
+    caption = f"""
+        \n Hare Krishna \n
+         Original Verse: \n
+            {sanskrit_text}
+        \n
+        Hindi Translation:\n
+            {hindi_text}
+        \n \n
+    """
+
+    create_image_post(text)
+
+    print("Image created")
+    bot = MyIGBot("iiradhakrishnaii", "BankeBihari100")
+    response = bot.upload_post(
+        "bhagavad_gita_api/media/images/output.jpg", caption=caption
+    )
+    print(response)  # if the response code is 200 that means ok
+
+    return 200
+
+
+def create_image_post(text):
+    """
+    usign pillow to add text on an image template, adjusting font
+    size and line width to avoid overflows
+    """
+    img = Image.open("bhagavad_gita_api/media/images/template.jpg")
+    draw = ImageDraw.Draw(img)
+    font_size = 40
+    font = ImageFont.truetype("bhagavad_gita_api/media/helveticaneue.ttf", font_size)
+
+    lines = wrap(text=text, width=50)
+
+    line_width, line_height = font.getsize(lines[0])
+
+    text_height = len(lines) * line_height
+
+    while text_height > 250:
+        print("in the loop")
+        font_size -= 5
+        font = ImageFont.truetype(
+            "bhagavad_gita_api/media/helveticaneue.ttf", font_size
+        )
+        line_height -= 10
+        text_height = len(lines) * line_height
+
+    print(font_size)
+    y_text = 805
+
+    image_width, image_height = img.size
+
+    for line in lines:
+        line_width, line_height = font.getsize(line)
+        draw.text(
+            ((image_width - line_width) / 2, y_text), line, font=font, fill=(0, 0, 0)
+        )
+        y_text += line_height
+    rgb_im = img.convert("RGB")
+    rgb_im.save("bhagavad_gita_api/media/images/output.jpg")
+
+
+def create_video_post():
+    pass
